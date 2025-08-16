e84d52f9b199d261c6b833f8c814101d13cf6b99
ntrngyn <krakenraz3@gmail.com>
2025-08-16 22:31:34 +0700

    refactor: Switch to HashRouter for GitHub Pages compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,13 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
-import './assets/global.css';
+import { HashRouter } from 'react-router-dom'; // <-- 1. Thay đổi import
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter> {/* <-- 2. Thay đổi component ở đây */}
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
-)
+);
