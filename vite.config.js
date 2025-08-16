26c7a29597a0ec359904248bf8c49869e834d763
ntrngyn <krakenraz3@gmail.com>
2025-08-16 20:10:43 +0700

    chore: Configure for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,15 @@
 // vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import basicSsl from '@vitejs/plugin-basic-ssl'; // <-- 1. Import plugin
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import basicSsl from "@vitejs/plugin-basic-ssl"; // <-- 1. Import plugin
 
 export default defineConfig({
+  base: "/ntrngyn-bakerz-bite-eproject/",
   plugins: [
     react(),
-    basicSsl() // <-- 2. Thêm plugin vào mảng plugins
+    basicSsl(), // <-- 2. Thêm plugin vào mảng plugins
   ],
   server: {
     https: true, // <-- 3. Bật chế độ HTTPS
-  }
+  },
 });
