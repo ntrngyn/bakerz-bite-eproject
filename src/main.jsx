74157589b4b94e31248136b8f9c437bd1a36c3c1
ntrngyn <krakenraz3@gmail.com>
2025-08-16 23:15:05 +0700

    fix: Restore missing global CSS import

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import { HashRouter } from 'react-router-dom'; // <-- 1. Thay đổi import
-import './index.css';
+import './assets/global.css';
+// import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
